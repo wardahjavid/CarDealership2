8e2a897c2bb3c8322481abeb35b1403a6e874864
Student <wardahjavid62@gmail.com>
2025-11-03 11:23:47 -0500

    Adding Ansi color code to ContractFileManager class.

---
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ContractFileManager {
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RESET = "\u001B[0m";
+}
