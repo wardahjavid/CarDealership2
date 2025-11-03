6e6fad281dd8be6a99548a825de749d23f317887
Student <wardahjavid62@gmail.com>
2025-11-03 02:19:51 -0500

    Create Constructor for Vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -9,4 +9,15 @@ public class Vehicle {
     private String color;
     private int odometer;
     private double price;
+
+    public Vehicle(int vehicleId, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vehicleId = vehicleId;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
