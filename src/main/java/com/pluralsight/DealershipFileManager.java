a15bffabcec54bc62b0a4d5b2e1741fb3cb3f9a5
Student <wardahjavid62@gmail.com>
2025-11-03 02:41:05 -0500

    public Dealership getDealership opens the dealership.csv file and reads it line by line. It starts by creating an empty Dealership variable and an ArrayList to hold Vehicle objects. This happens in the DealershipFileManager class.

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,11 +1,20 @@
 package com.pluralsight;
 
 
+import java.util.ArrayList;
+
 public class DealershipFileManager {
+
+    // ANSI color codes for console formatting
     public static final String RESET = "\u001B[0m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     public static final String RED = "\u001B[31m";
     public static final String CYAN = "\u001B[36m";
 
+    public Dealership getDealership() {
+        Dealership dealership = null;
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        System.out.println(YELLOW + "\n LOADING DEALERSHIP DATA " + RESET);
+    }
 }
